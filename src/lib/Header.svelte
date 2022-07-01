a690b7f0bae04828e83f97038a85151c41f3d32f
MaskedTrench <markushasorokin@yandex.com>
2022-07-01 16:15:24 +0300

    WIP: Storybook, sass

---
+++ src/lib/Header.svelte
@@ -0,0 +1,7 @@
+<header>
+    <div class="container">
+        <a href="/">
+            <p><i class="bi-code"/> Logo</p>
+        </a>
+    </div>
+</header>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,6 @@
+<section>
+    <div class="container">
+        <h1>404</h1>
+        <p>Page not found</p>
+    </div>
+</section>
